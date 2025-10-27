038edbe57afb22d1b087be383abf44f8fb0efea4
git config --global user.name <dineshmeij@gmail.com>
2025-10-27 23:08:05 +0530

    5.0

--- src/test/java/runners/TEST_RUNNER.java
+++ src/test/java/runners/TEST_RUNNER.java
@@ -11,24 +11,28 @@ import org.junit.runner.RunWith;
 @CucumberOptions(features = "./src/test/resources/feature_files",
 
 
-        glue = { "StepDefinition" ,"Helpers"},
+        glue = {"StepDefinition", "Helpers"},
 
         dryRun = false,
         monochrome = true,
         snippets = CucumberOptions.SnippetType.UNDERSCORE,
-        plugin = { "pretty",
+        plugin = {"pretty",
 
                 "html:target/cucumber-reports/report.html",
                 "json:target/cucumber-reports/cucumber.json",
 
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-                "rerun:target/failed_scenarios.txt" }, // just chek failled sceainer faile name save to the text file
+                "rerun:target/failed_scenarios.txt"}, // just chek failled sceainer faile name save to the text file
         publish = true,
         tags = ("@try")
 
 )
-public class TEST_RUNNER extends  AbstractTestNGCucumberTests{
+public class TEST_RUNNER extends AbstractTestNGCucumberTests {
 
-
+    // mvn test ---->only run test without Generates Reports
+    // mvn clean ---->Deletes all existing files
+    //mvn verify ----->Generates all Reports
+    // mvn clean test ----> Cleans old data, then runs all tests fresh (no reports yet).
+    //mvn clean test verify ----> Full workflow: cleans old data → runs tests → generates all reports.
 
 }
